4e265b186ec698d69f3cf98558b92e0138809ccc
Mohit <53659907+MohitGohil@users.noreply.github.com>
2021-06-19 16:07:33 +0530

    schema
    article schema changed

--- models/article.js
+++ models/article.js
@@ -2,14 +2,13 @@ const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema
 
-// type validation
 const articleSchema = new Schema({
     title: {type: String, required: true},
     body: {type: String, required: true},
     author: {type: String, required: true}
 }, {timestamps: true})
 
-// Storing articles in collection(Article, schema)
+// applying schema 
 const Article = mongoose.model('Article', articleSchema)
 
 module.exports = Article
